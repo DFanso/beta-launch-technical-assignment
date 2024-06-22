6056e52858e02563084d842ee4ddd7f30eaaf4e4
Leo Gavin <leogavin123@outlook.com>
2024-06-22 09:57:09 +0530

    feat: validation pipeline

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,60 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { useRequestLogging } from './request-logging';
+import {
+  BadRequestException,
+  ValidationPipe,
+  VersioningType,
+} from '@nestjs/common';
+import * as basicAuth from 'express-basic-auth';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(5000);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    rawBody: true,
+    bodyParser: true,
+  });
+  useRequestLogging(app);
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      exceptionFactory: (errors) => {
+        const messages = errors.map((error) => ({
+          property: error.property,
+          constraints: error.constraints,
+        }));
+        return new BadRequestException(messages);
+      },
+    }),
+  );
+
+  app.use(
+    '/api',
+    basicAuth({
+      challenge: true,
+      users: {
+        admin: 'admin',
+      },
+    }),
+  );
+  const config = new DocumentBuilder()
+    .setTitle('EMS API')
+    .setDescription('Employment Management system Backend API')
+    .setVersion('1.0')
+    .addTag('EMS')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  app.enableCors();
+
+  await app.listen(9000);
 }
 bootstrap();
